20a549a50ade0fa59b6fd8a5672f257d90b0bdc6
dmytr <dmitriu564@gmail.com>
2021-12-05 20:58:14 +0200

    Empty request on register validation fix

--- src/modules/auth/guards/user-exist.guard.ts
+++ src/modules/auth/guards/user-exist.guard.ts
@@ -1,15 +1,26 @@
 import {
-    CanActivate, ExecutionContext, ForbiddenException, Injectable,
+    BadRequestException, CanActivate, ExecutionContext, ForbiddenException, Injectable,
 } from '@nestjs/common';
+import {validate} from 'class-validator';
 import {Observable} from 'rxjs';
 import {UsersService} from 'src/modules/public/services/users.service';
+import {CreateUserDTO} from '../dto/create-user.dto';
 
 @Injectable()
 export class DoesUserExist implements CanActivate {
     constructor(private readonly userService: UsersService) {}
 
     async validateRequest(req): Promise<boolean> {
-        const userExist = await this.userService.getUserByEmail(req.body.email);
+        const createUser = new CreateUserDTO();
+        createUser.login = req.body.login;
+        createUser.password = req.body.password;
+        const errors = await validate(createUser);
+
+        if (errors.length) {
+            throw new BadRequestException(errors);
+        }
+
+        const userExist = await this.userService.getUserByLogin(req.body.login);
         if (userExist) {
             throw new ForbiddenException('Email already in use');
         }
